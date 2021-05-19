1648691ac50851a90161e87296f80bf51f04cdfe
CiomaRoland <cioma.roland.sandor@student.ms.sapientia.ro>
2021-05-19 18:36:46 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Projekt3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt3.dir/main.c.obj"
+  "Projekt3.exe"
+  "Projekt3.exe.manifest"
+  "Projekt3.pdb"
+  "libProjekt3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projekt3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
